3a3f7a8ccbebf78b2ea79d32fea838269a01543f
rachitjha09 <rachitjha09@gmail.com>
2024-09-02 19:28:46 -0400

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,15 @@
+
+/**
+ * Demo of Math Class
+ *
+ * @author (Rachit Jha)
+ * @version (9/26/23)
+ */
+public class Demo
+{
+    public static void main(String[]args){
+        double x=10.0;
+        double cubeRoot=Math.pow(x, 1.0/3);
+        
+    }
+}
